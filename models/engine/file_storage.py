c08ddc10a9cd80566e452349ebe78f98d2336d42
root <gumquat@gmail.com>
2023-06-09 14:10:11 -0700

    :P

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+import json
+import os
 
 class FileStorage:
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         #if new instance add
         # a call to the method: new(self) on 'storage'
-        self.__file_path = # stringpath to json file
-        self.__objects = # store obj as <class name>.id
+        self.__file_path = ""
+        self.__objects = []
 
     def all(self):
         return self.__objects
@@ -13,9 +15,26 @@ class FileStorage:
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
-        
+
     def save(self):
-        # serializes __objects to json filepath if it exists
+        ser_obj = {}
+        for key, obj in self.__objects.items():
+            ser_obj[key] = obj.to_dict()
     
-    def reload(save):
-        #deseriealizes the json file to __objects if it exists
+        with open(self.__file_path, 'w') as file:
+            json.dump(ser_obj, file)
+
+        #create a string representation of the saved filepath
+        #saved to __file_path
+    
+    def reload(self):
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                objects = json.load(file)
+
+            for key, des_obj in objects.items():
+                class_name, obj_id = key.split('.')
+                model = __import__('models.' + class_name, fromlist=[class_name])
+                cls = getattr(model, class_name)
+                obj = cls(**des_obj)
+                self.__objects[key] = obj
