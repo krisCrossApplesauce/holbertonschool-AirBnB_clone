61145311e11e5343d0bf33bf5c4ac10863d65686
root <gumquat@gmail.com>
2023-06-09 12:41:05 -0700

    :P

---
+++ tests/base_model_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Base Model Test #3"""
+import unittest
+from models.base import base_model
+from unittest.mock import patch
+from io import StringIO
+
+class TestBase(unittest.TestCase):
+    """TEST: BASE CLASS TESTS"""
+    def test_safe()
+        
