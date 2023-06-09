24e12e5def0bbdde36815d95526fb0558c5caa6c
krisCrossApplesauce <karisrr2203@gmail.com>
2023-06-09 12:37:09 -0700

    models/base_model.py: args and kwargs in __init__ (#4)

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,17 @@ from datetime import datetime
 
 class BaseModel:
     """ the base class for all other classes in this project """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
