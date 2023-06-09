387bc98d1b7cd3fc3dda71baa842109b084717cf
krisCrossApplesauce <karisrr2203@gmail.com>
2023-06-09 12:01:22 -0700

    models/base_model.py: in to_dict: self.__dict__copy() -> self.__dict__.copy() and .isoformat() -> .isoformat(''T'', ''microseconds'')

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,8 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        obj_dict = self.__dict__copy()
+        obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
-        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+        obj_dict['created_at'] = obj_dict['created_at'].isoformat("T", "microseconds")
+        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat("T", "microseconds")
         return obj_dict
