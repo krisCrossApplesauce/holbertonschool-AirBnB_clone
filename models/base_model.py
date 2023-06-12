3bf4d67824c2fcc222733225b8a86804969bcb51
krisCrossApplesauce <karisrr2203@gmail.com>
2023-06-12 13:09:19 -0700

    [base_model.py] __init__: storage.save() -> storage.new(self) -_-

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,13 @@ class BaseModel:
                     if key == "created_at" or key == "updated_at":
                         value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
+                if "id" not in kwargs:
+                    self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.save()
+            storage.new(self)
 
     def __str__(self):
         """represent func in string format"""
