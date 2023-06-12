e25db09cf94b3f09bca6f55cfaeee6c7e886d6e5
root <gumquat@gmail.com>
2023-06-12 11:58:38 -0700

    :P

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,7 @@ import os
 from models.base_model import BaseModel
 
 class FileStorage:
-    """ read/write and serialization for json storage """
+    """read/write and serialization for json storage"""
     def __init__(self):
         self.__objects = dict()
         self.__file_path = "file.json"
@@ -14,7 +14,7 @@ class FileStorage:
     def all(self):
         """return dict of __objects"""
         return self.__objects
-    
+
     def new(self, obj):
         """add a new obj to __objects"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
@@ -25,10 +25,10 @@ class FileStorage:
         ser_obj = {}
         for key, obj in self.__objects.items():
             ser_obj[key] = obj.to_dict()
-    
+
         with open(self.__file_path, 'w') as file:
             json.dump(ser_obj, file)
-    
+
     def reload(self):
         """deseriealize json file into __objects if it exists"""
         if os.path.exists(FileStorage.__file_path):
@@ -36,7 +36,7 @@ class FileStorage:
                 for key, obj_data in json.load(fp).items():
                     if key not in FileStorage.__objects:
                         cls = FileStorage.__models[obj_data["__class__"]]
-                        FileStorage.__objects[k] = cls(**obj_data)
+                        FileStorage.__objects[key] = cls(**obj_data)
         else:
             with open(FileStorage.__file_path, 'w') as fp:
                 fp.write("{}")
