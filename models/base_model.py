8f8039945570043446f961ff6aa1219e8eddff71
root <gumquat@gmail.com>
2023-06-12 12:20:30 -0700

    :P

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,17 @@ from models import storage
 class BaseModel:
     """ the base class for all other classes in this project """
     def __init__(self, *args, **kwargs):
-        if kwargs and len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key != "__class__":
+        if kwargs:
+                if "__class__" in kwargs:
+                    del kwargs["__class__"]
+                for key, value in kwargs.items():
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.save()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """represent func in string format"""
