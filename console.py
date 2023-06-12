4d6bbd72e7a00eac1b7fbc27667076e4599797a1
gumquat <gumquat@gmail.com>
2023-06-12 17:02:00 -0500

    Update console.py - evan
    wtf is happening with github? cant push manually

--- console.py
+++ console.py
@@ -1,44 +1,73 @@
 #!/usr/bin/python3
 """ console """
 import cmd
+from models.models import models_dict
+from model.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ console """
     prompt = '(hbnb)'
 
-    def do_hello(self, arg):
+    def do_hello(self, line):
         """Prints a greeting message"""
         print("Hello, world!")
 
-    def do_quit(self, arg):
-        """Exits the shell"""
+    def do_quit(self, line):
+        """Exits the shell ... EOF"""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """Exits the shell"""
         return True
 
     # Override emptyline() to avoid repeating the last command
     def emptyline(self):
+        """empty line + 'enter' executes nothing"""
         pass
 
-    # def help
+    # def help [this is autoincluded]
 
-    # def create
+    def do_create(self, arg):
+        """creates new inst of BaseModel saved to json file and prints ID"""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in models_dict.keys():
+            print("** class doesn't exist **")
+        else:
+            new_model = models_dict[arg]()
+            new_model.save()
+            print(new_model.id)
+
+    def do_show(self, arg):
+        """print string rep of instance"""
+        args = arg.split()
+        class_name = args[0]
+        instance_id = args[1]
+        obj_dict = models_dict.storage.all() # double check this!
+
+        if not args:
+            print("** class name missing **")
+            return
+        if class_name not in models_dict:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = (f"{class_name}.{instance_id}")
+        if key not in obj_dict:
+            print("** no instance found **")
+            return
+        print(obj_dict[key])
 
     # def show
 
-    def do_destroy(self, line):
-        if line is not None and line != "":
-            args = line.split()
-        else:
-            print("** class name missing **")
+    # def destroy
 
-    def do_all(self, line):
-        pass
+    # all
 
-    # def update
+    # update
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
