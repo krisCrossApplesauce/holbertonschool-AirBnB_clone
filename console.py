9f798c7ef18c25f62ddb73f15ee6cd97b361f71b
root <gumquat@gmail.com>
2023-06-13 14:07:02 -0700

    :P

--- console.py
+++ console.py
@@ -103,6 +103,5 @@ class HBNBCommand(cmd.Cmd):
             models.storage.update(models.storage.all()[f"{args[0]}.{args[1]}"]
                                 args[2], args[3])
 
-
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
